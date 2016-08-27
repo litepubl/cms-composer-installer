5dbe46b99c6846bbb527122991d34072fff96295
litepubl <yarrowsoft@gmail.com>
2016-08-27 11:56:29 +0300

    in progress

--- Composer/Plugin.php
+++ Composer/Plugin.php
@@ -7,17 +7,18 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 class Plugin implements PluginInterface
- {
+{
 
-	/**
-	 * Apply plugin modifications to composer
-	 *
-	 * @param Composer    $composer
-	 * @param IOInterface $io
-	 */
-	public function activate( Composer $composer, IOInterface $io ) {
-		$installer = new Installer( $io, $composer );
-		$composer->getInstallationManager()->addInstaller( $installer );
-	}
+    /**
+     * Apply plugin modifications to composer
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function activate( Composer $composer, IOInterface $io ) 
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
 
 }
